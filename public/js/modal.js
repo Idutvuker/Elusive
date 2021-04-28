a25dcf1e2b7ff70b3a58d329c5b2bd7564877ad6
Nikolay <smir.nick2000@gmail.com>
2021-04-28 18:30:10 +0300

    Implement login and register

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,25 +1,20 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("mySignUp");
+let close = document.getElementById("cancel");
 
-// Get the button that opens the modal
-var btn = document.getElementById("mySignUp");
 
-// Get the <span> element that closes the modal
-var close = document.getElementById("cancel");
-
-// When the user clicks on the button, open the modal
 btn.onclick = function() {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+
 close.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target === modal) {
         modal.style.display = "none";
     }
 }
